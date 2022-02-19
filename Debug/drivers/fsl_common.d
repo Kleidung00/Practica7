e645c15c93cdbc1782e45bd7ee4b86fd6b1de20a
ClaudioCastro <claudioleoncastro2000@gmail.com>
2022-02-19 13:17:35 -0600

    Todos los archivos

---
+++ Debug/drivers/fsl_common.d
@@ -0,0 +1,25 @@
+drivers/fsl_common.o drivers/fsl_common.d: ../drivers/fsl_common.c \
+ ../drivers/fsl_common.h \
+ C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/fsl_device_registers.h \
+ C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/MKL25Z4.h \
+ C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/core_cm0plus.h \
+ C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/core_cmInstr.h \
+ C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/cmsis_gcc.h \
+ C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/core_cmFunc.h \
+ C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/system_MKL25Z4.h \
+ C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/MKL25Z4_features.h \
+ ../drivers/fsl_clock.h \
+ C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\utilities/fsl_debug_console.h \
+ C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\drivers/fsl_common.h
+../drivers/fsl_common.h:
+C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/fsl_device_registers.h:
+C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/MKL25Z4.h:
+C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/core_cm0plus.h:
+C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/core_cmInstr.h:
+C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/cmsis_gcc.h:
+C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/core_cmFunc.h:
+C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/system_MKL25Z4.h:
+C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\CMSIS/MKL25Z4_features.h:
+../drivers/fsl_clock.h:
+C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\utilities/fsl_debug_console.h:
+C:\Users\claud\Documents\MCUXpressoIDE_11.5.0_7232\workspace\HELLO_demo_apps_hello_world\drivers/fsl_common.h:
